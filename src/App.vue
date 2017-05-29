96c2c0a1194f6fab3dff7c98a03f60e66556092b
winters0991 <499340367@qq.com>
2017-05-29 20:17:04 +0800

    first submit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class='page'>
+    <header>
+      <Topbar/>
+    </header>
+    <main>
+      <ResumeEditor/>
+      <ResumePreview/>
+    </main>
+  </div>
+</template>
+
+<script>
+
+import 'normalize.css/normalize.css'
+import './assets/reset.css'
+
+import Topbar from './components/Topbar'
+import ResumeEditor from './components/ResumeEditor'
+import ResumePreview from './components/ResumePreview'
+
+export default {
+  name: 'app',
+  data: function(){
+    return {
+      text: '你好'
+    }
+  },
+  components: {Topbar, ResumeEditor, ResumePreview}
+}
+</script>
+
+<style>
+  .page {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    background: #EAEBEC;
+  }
+  .page main {
+    flex-grow: 1;
+  }
+  .page main {
+    min-width: 1024px;
+    max-width: 1440px;
+    margin-top: 16px;
+    margin-bottom: 16px;
+    display: flex;
+    justify-content: space-between;
+    align-self: center;
+    width: 100%;
+    padding: 0 16px;
+  }
+
+#resumeEditor {
+  width: 35%;
+}
+#resumePreview {
+  width: 61.66667%;
+  flex-grow: 1;
+  margin-left: 16px;
+}
+
+</style>
