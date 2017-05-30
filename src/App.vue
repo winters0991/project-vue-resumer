d4e5806f46120f850fe4dcf5272481f576282752
winters0991 <499340367@qq.com>
2017-05-30 19:58:50 +0800

    bio重命名为profile,visibleItems重命名为config

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,7 @@ import './assets/reset.css'
 import Topbar from './components/Topbar'
 import ResumeEditor from './components/ResumeEditor'
 import ResumePreview from './components/ResumePreview'
+import icons from './assets/icons'
 
 export default {
   name: 'app',
@@ -26,7 +27,10 @@ export default {
       text: '你好'
     }
   },
-  components: {Topbar, ResumeEditor, ResumePreview}
+  components: {Topbar, ResumeEditor, ResumePreview},
+  created(){
+    document.body.insertAdjacentHTML('afterbegin', icons) //插入元素内部的第一个子节点之前
+  }
 }
 </script>
 
@@ -36,7 +40,7 @@ export default {
     display: flex;
     flex-direction: column;
     background: #EAEBEC;
-    
+
     >main {
       flex-grow: 1;
     }
--- src/components/ResumeEditor.vue
+++ src/components/ResumeEditor.vue
@@ -2,17 +2,14 @@
   <div id="resumeEditor">
     <nav>
       <ol>
-        <li v-for='(item,index) in resume.visibleItems'
-            :class='{active: item === selected}'
-            @click='selected = item'
-        >
+        <li v-for='(item,index) in resume.config' :class='{active: item === selected}' @click='selected = item'>
         {{index}}
         </li>
       </ol>
     </nav>
     <ol class="panels">
-      <li v-for='item in resume.visibleItems' v-show='item === selected'>
-        {{resume[item]}}
+      <li v-for='item in resume.config' v-show='item.field === selected'>
+        {{resume[item.field]}}
       </li>  
     </ol>
   </div>
@@ -23,10 +20,17 @@ export default {
   name: 'ResumeEditor',
   data() {
     return {
-      selected: 'bio',
+      selected: 'profile',
       resume: {
-        visibleItems: ['bio', 'work history', 'education', 'projects', 'awards', 'contacts', 'others'],
-        bio: {
+        config: [
+          { field: 'profile', icon: 'id' },
+          { field: 'work history', icon: 'work' },
+          { field: 'education', icon: 'book' },
+          { field: 'projects', icon: 'heart' },
+          { field: 'awards', icon: 'cup' },
+          { field: 'contacts', icon: 'phone' }
+        ],
+        profile: {
           name: '',
           city: '',
           title: ''
@@ -35,8 +39,7 @@ export default {
         education: [],
         projects: [],
         awards: [],
-        contacts: [],
-        others: []
+        contacts: []
       }
     }
   }
